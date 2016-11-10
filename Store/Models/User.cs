0071c4069b047bd11cb9d4e50cd183073462611f
Andry <hsmel73@mail.ru>
2016-11-10 18:35:58 +0300

    Добавлена серверная часть Web Api 2

---
+++ Store/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Store.Models
+{
+  public class User
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+  }
+  public class UserContext : DbContext
+  {
+    public DbSet<User> Users { get; set; }
+  }
+  public class UsersDbInitializer : DropCreateDatabaseAlways<UserContext>
+  {
+    protected override void Seed(UserContext db)
+    {
+      db.Users.Add(new User { Name = "Война и мир" });
+      db.Users.Add(new User { Name = "Отцы и дети" });
+      db.Users.Add(new User { Name = "Чайка" });
+      base.Seed(db);
+    }
+  }
+}
